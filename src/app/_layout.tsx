bf7e66be9b16df1f38db2e90f294f721de66b823
Ayush Singh <ayushsinghbharti123@gmail.com>
2024-05-04 17:53:04 +0530

    installed steamChat SDK

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,11 @@
 //Define Global provider eg. authentication etc..
-import { Slot, Stack, Tabs } from "expo-router"
+import { Slot, Stack, Tabs } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function Rootlayout(){
-    return <Slot />
+export default function Rootlayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slot />
+    </GestureHandlerRootView>
+  );
 }
