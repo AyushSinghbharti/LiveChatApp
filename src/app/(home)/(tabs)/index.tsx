36c822eed486f3a1e259e6762d83ede46462fe6a
Ayush Singh <ayushsinghbharti123@gmail.com>
2024-05-06 02:33:17 +0530

    Stream UI fixed

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { ChannelList } from "stream-chat-expo";
+import { router } from "expo-router";
 
 export default function MainScreen() {
   return (
-    <View>
-      <Text>Main Tab</Text>
-    </View>
+    <ChannelList
+      onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
+    />
   );
 }
