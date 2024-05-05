44365cdd5a4590a77d35944063e65ae7a9a35753
Ayush Singh <ayushsinghbharti123@gmail.com>
2024-05-06 03:20:00 +0530

    Initialized Stream chat provider

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,39 +1,12 @@
-import { Stack, Slot } from "expo-router";
-import { useEffect } from "react";
-import { Platform } from "react-native";
-import { StreamChat } from "stream-chat";
-import { Chat as ExpoChat, OverlayProvider } from "stream-chat-expo";
-
-const client = StreamChat.getInstance("949yy4smtek7");
+import { Stack } from "expo-router";
+import ChatProvider from "../providers/ChatProvider";
 
 export default function HomeLayout() {
-  useEffect(() => {
-    const connect = async () => {
-      await client.connectUser(
-        {
-          id: "jlahey",
-          name: "Jim Lahey",
-          image: "https://i.imgur.com/fR9Jz14.png",
-        },
-        client.devToken("jlahey")
-      );
-
-      //   const channel = client.channel("messaging", "the_park", {
-      //     name: "The Park",
-      //   });
-      //   await channel.watch();
-    };
-
-    connect();
-  });
-
   return (
-    <OverlayProvider>
-      <ExpoChat client={client}>
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{headerShown: false}} />
-        </Stack>
-      </ExpoChat>
-    </OverlayProvider>
+    <ChatProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ChatProvider>
   );
 }
