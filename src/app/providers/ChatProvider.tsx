b2ada3e62de75c52feb3253fb2b18767f9ec71c4
Ayush Singh <ayushsinghbharti123@gmail.com>
2024-05-07 00:31:45 +0530

    fixing user DP while login

--- src/app/providers/ChatProvider.tsx
+++ src/app/providers/ChatProvider.tsx
@@ -1,73 +1,11 @@
-// import { PropsWithChildren } from "react";
-// import { useEffect } from "react";
-// import { StreamChat } from "stream-chat";
-// import { Chat, OverlayProvider } from "stream-chat-expo";
-// import { useState } from "react";
-// import { ActivityIndicator } from "react-native";
-// import { useAuth } from "./AuthProvider";
-
-// const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
-
-// export default function ChatProvider({ children }: PropsWithChildren) {
-//   const [isReady, setIsReady] = useState(false);
-//   const {profile} = useAuth();
-
-//   useEffect(() => {
-//     console.log("USE EFFECT", profile);
-//     if(!profile) return;
-//     const connect = async () => {
-//       await client.connectUser(
-//         {
-//           id: "ayush",
-//           name: "ayush_com",
-//           image: "https://i.imgur.com/fR9Jz14.png",
-//         },
-//         client.devToken("ayush")
-//       );
-//       // await client.connectUser(
-//       //   {
-//       //     id: profile.id,
-//       //     name: profile.full_name,
-//       //     image: "https://i.imgur.com/fR9Jz14.png",
-//       //   },
-//       //   client.devToken(profile.id)
-//       // );
-//       setIsReady(true);
-
-//       //   const channel = client.channel("messaging", "the_park", {
-//       //     name: "The Park",
-//       //   });
-//       //   await channel.watch();
-//     };
-
-//     connect();
-//     return () => {
-//       if(isReady)
-//         client.disconnectUser();
-//         setIsReady(false);
-//     }
-//   }, []);
-
-//   if (!isReady) {
-//     return <ActivityIndicator style={{flex: 1, justifyContent: 'center'}} size={50} />;
-//   }
-
-//   return (
-//     <>
-//       <OverlayProvider>
-//         <Chat client={client}>{children}</Chat>
-//       </OverlayProvider>
-//     </>
-//   );
-// }
-
-import { PropsWithChildren, useEffect, useState } from "react";
-import { ActivityIndicator } from "react-native";
+import { PropsWithChildren } from "react";
+import { useEffect } from "react";
 import { StreamChat } from "stream-chat";
 import { Chat, OverlayProvider } from "stream-chat-expo";
+import { useState } from "react";
+import { ActivityIndicator } from "react-native";
 import { useAuth } from "./AuthProvider";
-// import { supabase } from '../lib/supabase';
-// import { tokenProvider } from '../utils/tokenProvider';
+import { supabase } from "../../lib/supabase";
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
 
@@ -76,15 +14,14 @@ export default function ChatProvider({ children }: PropsWithChildren) {
   const { profile } = useAuth();
 
   useEffect(() => {
-    if (!profile) {
-      return;
-    }
+    if (!profile) return;
     const connect = async () => {
       await client.connectUser(
         {
           id: profile.id,
           name: profile.full_name,
-          image: "https://i.imgur.com/fR9Jz14.png",
+          image: supabase.storage.from("avatars").getPublicUrl(profile.avatar)
+            .data.publicUrl,
         },
         client.devToken(profile.id)
       );
@@ -92,22 +29,77 @@ export default function ChatProvider({ children }: PropsWithChildren) {
     };
 
     connect();
-
     return () => {
-      if (isReady) {
-        client.disconnectUser();
-      }
+      if (isReady) client.disconnectUser();
       setIsReady(false);
     };
-  }, [profile?.id]);
+  }, []);
 
   if (!isReady) {
-    return <ActivityIndicator style={{flex: 1, justifyContent: 'center'}} size={50} />;
+    return (
+      <ActivityIndicator
+        style={{ flex: 1, justifyContent: "center" }}
+        size={50}
+      />
+    );
   }
 
   return (
-    <OverlayProvider>
-      <Chat client={client}>{children}</Chat>
-    </OverlayProvider>
+    <>
+      <OverlayProvider>
+        <Chat client={client}>{children}</Chat>
+      </OverlayProvider>
+    </>
   );
 }
+
+// import { PropsWithChildren, useEffect, useState } from "react";
+// import { ActivityIndicator } from "react-native";
+// import { StreamChat } from "stream-chat";
+// import { Chat, OverlayProvider } from "stream-chat-expo";
+// import { useAuth } from "./AuthProvider";
+// // import { supabase } from '../lib/supabase';
+// // import { tokenProvider } from '../utils/tokenProvider';
+
+// const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
+
+// export default function ChatProvider({ children }: PropsWithChildren) {
+//   const [isReady, setIsReady] = useState(false);
+//   const { profile } = useAuth();
+
+//   useEffect(() => {
+//     if (!profile) {
+//       return;
+//     }
+//     const connect = async () => {
+//       await client.connectUser(
+//         {
+//           id: profile.id,
+//           name: profile.full_name,
+//           image: supabase.storage.from('bucket').getPublicUrl('filePath.jpg'),
+//         },
+//         client.devToken(profile.id)
+//       );
+//       setIsReady(true);
+//     };
+
+//     connect();
+
+//     return () => {
+//       if (isReady) {
+//         client.disconnectUser();
+//       }
+//       setIsReady(false);
+//     };
+//   }, [profile?.id]);
+
+//   if (!isReady) {
+//     return <ActivityIndicator style={{flex: 1, justifyContent: 'center'}} size={50} />;
+//   }
+
+//   return (
+//     <OverlayProvider>
+//       <Chat client={client}>{children}</Chat>
+//     </OverlayProvider>
+//   );
+// }
